73c12878155446f78050167ccc38bc7560cda67f
Adam Ladachowski <adam@roomzilla.net>
2021-01-28 19:33:59 +0100

    Actually use expo

--- App.js
+++ App.js
@@ -1,51 +1,88 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from 'react'
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native'
 
-const App = () => {
+export default function App() {
   const [moneyWastedSoFar, setMoney] = useState(0)
   const [isWasting, setIsWasting] = useState(false)
-  const [isPaused, setIsPaused] = useState(false)
+  const [isNotWasting, setIsNotWasting] = useState(false)
   const increment = useRef(null)
+  const [buttonLabel, setLabel] = useState('Start')
 
   const usdPerHour = 25
 
-  const handleStart = () => {
+  let startWasting = () => {
     setIsWasting(true)
-    setIsPaused(true)
+    setIsNotWasting(true)
     increment.current = setInterval(() => {
       setMoney((moneyWastedSoFar) => moneyWastedSoFar + usdPerHour / 60)
     }, 1000)
   }
 
-  const handlePause = () => {
+  let pauseWasting = () => {
     clearInterval(increment.current)
-    setIsPaused(false)
+    setIsNotWasting(false)
   }
 
-  const formatCurrency = () => {
+  let formatCurrency = () => {
     return `$ ${moneyWastedSoFar.toFixed(2)}`
   }
 
+  let toggleWasting = () => {
+    if (!isWasting && !isNotWasting) {
+      startWasting()
+      setLabel('Pause')
+    } else if (isNotWasting) {
+      pauseWasting()
+      setLabel('Resume')
+    } else {
+      setLabel('Pause')
+      startWasting()
+    }
+  }
+
   return (
-    <div className="app">
-      <div className='money-wasted'>
-        <p>
-          As a develper, earning {usdPerHour} bucks per hour
-          I want to know how much my meetings cost my employer:
-        </p>
-        <p>{formatCurrency()}</p>
-        <div className='buttons'>
-          {
-            !isWasting && !isPaused ?
-              <button onClick={handleStart}>Start</button>
-              : (
-                isPaused ? <button onClick={handlePause}>Pause</button> :
-                  <button onClick={handleStart}>Resume</button>
-              )
-          }
-        </div>
-      </div>
-    </div>
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        As a develper, earning {usdPerHour} bucks per hour
+        I waste this much of my employers money on meetings
+      </Text>
+      <Text style={styles.currency}>
+        {formatCurrency()}
+      </Text>
+      <TouchableOpacity
+        onPress={() => toggleWasting()}
+        style={styles.button}>
+        <Text style={styles.buttonLabel}>{buttonLabel}</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  currency: {
+    fontSize: '48px',
+    margin: '24px'
+  },
+  buttonLabel: {
+    fontSize: '24px'
+  },
+  button: {
+    width: '200px',
+    border: '1px solid black',
+    textAlign: 'center',
+    borderRadius: '5px',
+    padding: '5px',
+    marginTop: '12px'
+  },
+  text: {
+    fontSize: '24px',
+    textAlign: 'center'
+  },
+  container: {
+    padding: '50px',
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
